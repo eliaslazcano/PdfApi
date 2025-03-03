0acd7c1c84b0526de724d6410b59773b8fd6f2bb
Elias Lazcano Castro Neto <elias304162@gmail.com>
2025-03-03 13:44:32 -0400

    Update imagem.php

--- public/imagem.php
+++ public/imagem.php
@@ -11,40 +11,44 @@ HttpHelper::validarPost();
 
 if (empty($_FILES)) HttpHelper::erroJson(400, 'Nenhum arquivo enviado');
 
-$arquivos = Utils::getAllUploadedFiles();
-if (count($arquivos) === 1) {
-  $pathTemporario = Utils::getPathTemporario('.pdf');
-  $file = reset($arquivos);
-  $sucesso = PdfUtils::converterImagem($file['tmp_name'], $pathTemporario);
-  if (!$sucesso) throw new Exception('O servidor não pôde gravar o arquivo comprimido.');
-
-  $nomeOriginal = pathinfo($file['tmp_name'], PATHINFO_FILENAME);
-
-  header('Content-Type: application/pdf');
-  header('Content-Length: ' . filesize($pathTemporario));
-  header("Content-Disposition: attachment; filename=$nomeOriginal.pdf");
-  readfile($pathTemporario);
-  unlink($pathTemporario);
-} else {
-  $pathsTemporarios = [];
-  foreach ($arquivos as $file) {
+try {
+  $arquivos = Utils::getAllUploadedFiles();
+  if (count($arquivos) === 1) {
     $pathTemporario = Utils::getPathTemporario('.pdf');
+    $file = reset($arquivos);
     $sucesso = PdfUtils::converterImagem($file['tmp_name'], $pathTemporario);
     if (!$sucesso) throw new Exception('O servidor não pôde gravar o arquivo comprimido.');
+
     $nomeOriginal = pathinfo($file['tmp_name'], PATHINFO_FILENAME);
-    $pathsTemporarios[] = ['path' => $pathTemporario, 'nome' => "$nomeOriginal.pdf"];
+
+    header('Content-Type: application/pdf');
+    header('Content-Length: ' . filesize($pathTemporario));
+    header("Content-Disposition: attachment; filename=$nomeOriginal.pdf");
+    readfile($pathTemporario);
+    unlink($pathTemporario);
+  } else {
+    $pathsTemporarios = [];
+    foreach ($arquivos as $file) {
+      $pathTemporario = Utils::getPathTemporario('.pdf');
+      $sucesso = PdfUtils::converterImagem($file['tmp_name'], $pathTemporario);
+      if (!$sucesso) throw new Exception('O servidor não pôde gravar o arquivo comprimido.');
+      $nomeOriginal = pathinfo($file['tmp_name'], PATHINFO_FILENAME);
+      $pathsTemporarios[] = ['path' => $pathTemporario, 'nome' => "$nomeOriginal.pdf"];
+    }
+
+    $zip = new ZipArchive();
+    $zipNome = Utils::getPathTemporario('.zip');
+    if (!$zip->open($zipNome, ZipArchive::CREATE)) throw new Exception('Não foi possível criar o arquivo ZIP.');
+    foreach ($pathsTemporarios as $i) $zip->addFile($i['path'], $i['nome']);
+    $zip->close();
+
+    foreach ($pathsTemporarios as $i) unlink($i['path']);
+    header('Content-Type: application/zip');
+    header('Content-Disposition: attachment; filename=imagens.zip');
+    header('Content-Length: ' . filesize($zipNome));
+    readfile($zipNome);
+    unlink($zipNome);
   }
-
-  $zip = new ZipArchive();
-  $zipNome = Utils::getPathTemporario('.zip');
-  if (!$zip->open($zipNome, ZipArchive::CREATE)) throw new Exception('Não foi possível criar o arquivo ZIP.');
-  foreach ($pathsTemporarios as $i) $zip->addFile($i['path'], $i['nome']);
-  $zip->close();
-
-  foreach ($pathsTemporarios as $i) unlink($i['path']);
-  header('Content-Type: application/zip');
-  header('Content-Disposition: attachment; filename=imagens.zip');
-  header('Content-Length: ' . filesize($zipNome));
-  readfile($zipNome);
-  unlink($zipNome);
+} catch (Exception $e) {
+  HttpHelper::erroJson(500, $e->getMessage());
 }
